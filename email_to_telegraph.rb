299e150a2d656382461c3cc97d234b4c16e39ac8
Patrick Schless <patrick@plainlystated.com>
2012-09-19 22:47:18 -0500

    script to run it all, via cron

--- email_to_telegraph.rb
+++ email_to_telegraph.rb
@@ -1,15 +1,22 @@
 require 'rubygems'
 require 'gmail'
 require 'yaml'
+require File.expand_path("./lib/morse_code.rb")
+require File.expand_path("./lib/telegraph_sounder.rb")
 
 config = YAML.load_file("config.yml")
 
 Gmail.new(config[:username], config[:password]) do |gmail|
+  puts Time.now
   puts "unread: #{gmail.inbox.count(:unread)}"
-
   gmail.inbox.find(:unread).each do |email|
-    puts email.subject
+    morse_code = MorseCode.encode(email.subject)
+    puts " - #{email.subject}"
+    puts "   #{morse_code}"
+
+    TelegraphSounder.say(morse_code)
     email.read!
   end
 end
+
 puts
